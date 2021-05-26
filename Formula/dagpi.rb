138d43ee8b10cbd0d6964326b13b31afa04e815e
Arnav Jindal <arnav.jindal7@gmail.com>
2021-05-26 18:05:48 +0530

    Update dagpi.rb

--- Formula/dagpi.rb
+++ Formula/dagpi.rb
@@ -3,7 +3,7 @@ class Dagpi < Formula
   homepage "https://github.com/daggy1234/dagpi-cli"
   url "https://github.com/daggy1234/dagpi-cli/archive/v0.3.1.tar.gz"
   sha256 "855808a6c97941c89b467870393fbba7372beed7445e983431290ab3f6250b16"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   depends_on "rust" => :build
